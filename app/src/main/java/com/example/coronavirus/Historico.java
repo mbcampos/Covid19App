fe150a6b03aa325b9644836f1033347f8971f6e5
Matheus Campos <matbcampos@gmail.com>
2020-05-14 06:45:14 -0300

    App

---
+++ app/src/main/java/com/example/coronavirus/Historico.java
@@ -0,0 +1,40 @@
+package com.example.coronavirus;
+
+import java.util.Date;
+
+class Historico {
+    private String country;
+    private Integer cases;
+    private Integer confirmed;
+    private Integer deaths;
+    private Integer recovered;
+    private Date updatedAt;
+
+    public void setCountry(String value) { this.country = value; }
+
+    public void setCases(Integer value) { this.cases = value; }
+
+    public void setConfirmed(Integer value) {
+        this.confirmed = value;
+    }
+
+    public void setDeaths(Integer value) {
+        this.deaths = value;
+    }
+
+    public void setRecovered(Integer value) {
+        this.recovered = value;
+    }
+
+    public String getCountry() { return this.country; }
+
+    public Integer getCases() { return this.cases; }
+
+    public Integer getConfirmed() {
+        return this.confirmed;
+    }
+
+    public Integer getDeaths() { return this.deaths; }
+
+    public Integer getRecovered() { return this.recovered; }
+}
---
+++ app/src/main/java/com/example/coronavirus/State.java
@@ -0,0 +1,43 @@
+package com.example.coronavirus;
+
+import java.util.Date;
+
+class State {
+    private String state;
+    private Integer cases;
+    private Integer deaths;
+    private Integer refuses;
+    private Integer suspects;
+    private Date updatedAt;
+
+    public void setState(String value) { this.state = value; }
+
+    public void setCases(Integer value) { this.cases = value; }
+
+    public void setDeaths(Integer value) {
+        this.deaths = value;
+    }
+
+    public void setRefuses(Integer value) {
+        this.refuses = value;
+    }
+
+    public void setSuspects(Integer value) {
+        this.suspects = value;
+    }
+
+    public String getState() { return this.state; }
+
+    public Integer getCases() { return this.cases; }
+
+    public Integer getDeaths() {
+        return this.deaths;
+    }
+
+    public Integer getRefuses() { return this.refuses; }
+
+    public Integer getSuspects() { return this.suspects; }
+
+    @Override
+    public String toString() { return this.state; }
+}
